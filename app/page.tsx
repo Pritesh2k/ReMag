8929d4136342344e27fa41042cf7312a97d863ae
Pritesh2k <priteshparekh237@gmail.com>
2026-01-09 13:47:07 +0000

    Initial Commit #2

---
+++ app/page.tsx
@@ -0,0 +1,40 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { CustomEase } from "gsap/CustomEase";
+
+// CustomBounce requires CustomEase
+import { CustomBounce } from "gsap/CustomBounce";
+
+// CustomWiggle requires CustomEase
+import { CustomWiggle } from "gsap/CustomWiggle";
+import { RoughEase, ExpoScaleEase } from "gsap/EasePack";
+    
+import { Draggable } from "gsap/Draggable";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { EaselPlugin } from "gsap/EaselPlugin";
+import { Flip } from "gsap/Flip";
+import { GSDevTools } from "gsap/GSDevTools";
+import { InertiaPlugin } from "gsap/InertiaPlugin";
+import { MotionPathHelper } from "gsap/MotionPathHelper";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { Observer } from "gsap/Observer";
+import { Physics2DPlugin } from "gsap/Physics2DPlugin";
+import { PhysicsPropsPlugin } from "gsap/PhysicsPropsPlugin";
+import { PixiPlugin } from "gsap/PixiPlugin";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// ScrollSmoother requires ScrollTrigger
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(useGSAP,Draggable,DrawSVGPlugin,EaselPlugin,Flip,GSDevTools,InertiaPlugin,MotionPathHelper,MotionPathPlugin,MorphSVGPlugin,Observer,Physics2DPlugin,PhysicsPropsPlugin,PixiPlugin,ScrambleTextPlugin,ScrollTrigger,ScrollSmoother,SplitText,RoughEase,ExpoScaleEase,CustomEase,CustomBounce,CustomWiggle);
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen">
+      
+    </div>
+  );
+}
